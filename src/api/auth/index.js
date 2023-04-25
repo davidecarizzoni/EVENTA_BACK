312a365ef7209bd5ed76d9707b392eeae5c1430b
davidecarizzoni <davide.carizzoni@runelab.it>
2023-04-25 19:06:11 +0200

    feat(googleLogin)

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 import { token, password } from './passport';
 import { actions } from '../users/controller';
-import { login } from './controller';
+import {googleLogin, login} from './controller';
 
 const router = new Router();
 
 router.post('/register', token({ required: false }), actions.create);
 router.post('/login', password(), login);
+router.post('/google', token({ required: false }), googleLogin);
 
 export default router;
